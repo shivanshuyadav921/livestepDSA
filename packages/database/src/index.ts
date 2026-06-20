3e52134cbc6d73afe5bfcdc684f48983d8ea3e48
shivanshuyadav921 <your-email@example.com>
2026-06-20 17:44:04 +0530

    fix database exports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-
+export { Stage } from '@prisma/client';
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
